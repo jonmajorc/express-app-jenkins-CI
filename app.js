e561cc2cf0d75fad787756cf98b5ab955d770bd2
Jon Major <jcondon@moxehealth.com>
2017-04-24 22:43:36 -0500

    Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',function(req,res){
-    res.send("Hello World")
+    res.send("Home Page")
 })
 
 app.get('/deployed',function(req,res){
